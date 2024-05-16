b89d8714317db8d249908f1f163dc665aacbf171
Lehlohonolo Monareng <lehlohonolomonareng@yahoo.com>
2024-05-16 08:40:43 +0200

    Updating nav and header concerns

--- public/index.js
+++ public/index.js
@@ -4,10 +4,10 @@ const navigation = document.querySelector('.navigation');
 // MOBILE TOGGLE NAVBAR
 
 const navLinks = document.getElementById("navLinks");
-function showMenu(){
-    navLinks.style.right= "0";
+function showMenu() {
+    navLinks.style.right = "0";
 }
-function hideMenu(){
+function hideMenu() {
     navLinks.style.right = "-200px";
 }
 
@@ -24,15 +24,15 @@ function hideMenu(){
 const form = document.querySelector("form");
 
 nextBtn = form.querySelector(".nextBtn"),
-backBtn = form.querySelector(".backBtn"),
-submitBtn = form.querySelector(".submitBtn"),
-allInput = form.querySelectorAll(".first input");
+    backBtn = form.querySelector(".backBtn"),
+    submitBtn = form.querySelector(".submitBtn"),
+    allInput = form.querySelectorAll(".first input");
 
 nextBtn.addEventListener("click", () => {
     allInput.forEach(input => {
-        if(input.value != ""){
+        if (input.value != "") {
             form.classList.add('secActive');
-        }else{
+        } else {
             form.classList.remove('secActive');
             alert("input is empty");
         }
--- public/registration-form.php
+++ public/registration-form.php
@@ -15,7 +15,7 @@
     </head>
     <body>
         <header>
-            <nav>
+            <nav class="section">
                 <a href="../index.html"><img src="images/logo2.png" alt=""></a>
                 <div class="nav-links" id="navLinks">
                     <i class="fa fa-times" onclick="hideMenu()"></i>
@@ -24,6 +24,7 @@
                         <li><a href="about.html">About</a></li>
                         <li><a href="subjects.html">Subjects</a></li>
                         <li><a href="https://claudemonareng.github.io/fine-point/">Blog</a></li>
+                        <li><a href="learning-portal.html">Learning Portal</a></li>
                         <li><a href="login.php">Admin Page</a></li>
                         <li><a href="contact-us.html">Contact Us</a></li>
                     </ul>
@@ -31,113 +32,108 @@
                 <i class="fa fa-bars" onclick="showMenu()"></i>
             </nav>
         </header>
-
         <div class="container">
-
+        
             <?php
                 include("connect.php");
                 include("code.php");
             ?>
-
-            <header>Online Registration</header>
-
+        
+            <h1>Online Registration</h1>
             <form action="code.php" method="post" enctype="multipart/form-data">
                 <div class="form first">
                     <div class="details personal">
                         <span class="title">Learner Personal Details</span>
-                        
                         <div class="fields">
                             <div class="input-field">
                                 <label>Grade Applied for</label>
-                                <input type="text" name="gradeAppliedFor" placeholder="Enter Grade" required >
+                                <input type="text" name="gradeAppliedFor" placeholder="Enter Grade">
                             </div>
                             <div class="input-field">
                                 <label>Highest Grade Passed</label>
-                                <input type="text" name="highestGradePassed" placeholder="Enter Highest Grade Passed" required >
+                                <input type="text" name="highestGradePassed" placeholder="Enter Highest Grade Passed">
                             </div>
                             <div class="input-field">
                                 <label>Year Grade Passed</label>
-                                <input type="text" name="yearWhenGradeWasPassed" placeholder="Enter year when Grade was Passed: " required >
+                                <input type="text" name="yearWhenGradeWasPassed" placeholder="Enter year when Grade was Passed: ">
                             </div>
                             <div class="input-field">
                                 <label>First Name</label>
-                                <input type="text" name="firstName" placeholder="Enter your first name" required >
+                                <input type="text" name="firstName" placeholder="Enter your first name">
                             </div>
                             <div class="input-field">
                                 <label>Last Name</label>
-                                <input type="text" name="lastName" placeholder="Enter your last name" required >
+                                <input type="text" name="lastName" placeholder="Enter your last name">
                             </div>
                             <div class="input-field">
                                 <label>Initials</label>
-                                <input type="text" name="initials" placeholder="Enter your initials" required >
+                                <input type="text" name="initials" placeholder="Enter your initials">
                             </div>
                         </div>
                     </div>
                     <div class="details id">
                         <span class="title">Learner Identity Details</span>
-
                         <div class="fields">
                             <div class="input-field">
                                 <label>Date of Birth</label>
-                                <input type="date" name="dateOfBirth" placeholder="Enter your date of birth" required >
+                                <input type="date" name="dateOfBirth" placeholder="Enter your date of birth">
                             </div>
                             <div class="input-field">
                                 <label>ID Number</label>
-                                <input type="text" name="idNumber" placeholder="Enter your ID number" required >
+                                <input type="text" name="idNumber" placeholder="Enter your ID number">
                             </div>
                             <div class="input-field">
                                 <label>Contact Number</label>
-                                <input type="text" name="contactNumber" placeholder="Enter your contact number" required >
+                                <input type="text" name="contactNumber" placeholder="Enter your contact number">
                             </div>
                             <div class="input-field">
                                 <label>Race</label>
-                                <input type="text" name="race" placeholder="Enter your race" required >
+                                <input type="text" name="race" placeholder="Enter your race">
                             </div>
                             <div class="input-field">
                                 <label>Province</label>
-                                <input type="text" name="province" placeholder="Enter your province" required >
+                                <input type="text" name="province" placeholder="Enter your province">
                             </div>
                             <div class="input-field">
                                 <label>Gender</label>
-                                <input type="text" name="gender" placeholder="Enter your gender" required >
+                                <input type="text" name="gender" placeholder="Enter your gender">
                             </div>
                             <div class="input-field">
                                 <label>Home Language</label>
-                                <input type="text" name="homeLanguage" placeholder="Enter your home language" required >
+                                <input type="text" name="homeLanguage" placeholder="Enter your home language">
                             </div>
                             <div class="input-field">
                                 <label>Residential Address</label>
-                                <input type="text" name="residentialAddress" placeholder="Residential address" required >
+                                <input type="text" name="residentialAddress" placeholder="Residential address">
                             </div>
                             <div class="input-field">
                                 <label>Residential Code</label>
-                                <input type="text" name="residentialCode" placeholder="Residential code" required >
+                                <input type="text" name="residentialCode" placeholder="Residential code">
                             </div>
                             <div class="input-field">
                                 <label>Learner Email Address</label>
-                                <input type="email" name="learnerEmailAddress" placeholder="Enter your email address" required >
+                                <input type="email" name="learnerEmailAddress" placeholder="Enter your email address">
                             </div>
                             <div class="input-field">
                                 <label>Preferred Language of Instruction</label>
-                                <input type="text" name="preferredLanguageOfInstruction" placeholder="Preferred language of instruction" required >
+                                <input type="text" name="preferredLanguageOfInstruction" placeholder="Preferred language of instruction">
                             </div>
                             <div class="input-field">
                                 <label>Deceased Parent</label>
-                                <input type="text" name="deceasedParent" placeholder="Enter deceased parent, if any" required >
+                                <input type="text" name="deceasedParent" placeholder="Enter deceased parent, if any">
                             </div>
                         </div>
                     </div>
                     <div class="details address">
                         <span class="title">Learner Identity Details</span>
-
                         <div class="fields">
                             <div class="input-field">
                                 <label>Dexterity of Learner</label>
-                                <input type="text" name="dexterityOfLearner" placeholder="left or right-handed or both" required >
+                                <input type="text" name="dexterityOfLearner" placeholder="left or right-handed or both">
                             </div>
                             <div class="input-field">
                                 <label>Do you have a hearing problem?</label>
-                                <input type="text" name="hearingProblem" placeholder="Yes or No" required >
+                                <input type="text" name="hearingProblem" placeholder="Yes or No">
                             </div>
                         </div>
                     </div>
@@ -149,109 +145,108 @@
                 <div class="form second">
                     <div class="parent details id">
                         <span class="title">Parent or Guardian Information</span>
-
                         <div class="fields">
                             <div class="input-field">
                                 <label>First Names</label>
-                                <input type="text" name="parentFirstNames" placeholder="Enter your firstnames" required >
+                                <input type="text" name="parentFirstNames" placeholder="Enter your firstnames">
                             </div>
                             <div class="input-field">
                                 <label>Surname</label>
-                                <input type="text`" name="parentSurname" placeholder="Enter your surname" required >
+                                <input type="text`" name="parentSurname" placeholder="Enter your surname">
                             </div>
                             <div class="input-field">
                                 <label>Title</label>
-                                <input type="text" name="parentTitle" placeholder="Enter your title" required >
+                                <input type="text" name="parentTitle" placeholder="Enter your title">
                             </div>
                             <div class="input-field">
                                 <label>Initials</label>
-                                <input type="text" name="parentInitials" placeholder="Enter your initials" required >
+                                <input type="text" name="parentInitials" placeholder="Enter your initials">
                             </div>
                             <div class="input-field">
                                 <label>ID Number</label>
-                                <input type="text" name="parentIdNumber" placeholder="Enter your ID Number" required >
+                                <input type="text" name="parentIdNumber" placeholder="Enter your ID Number">
                             </div>
                             <div class="input-field">
                                 <label>Contact Number</label>
-                                <input type="text" name="parentContactNumber" placeholder="Enter your contact number" required >
+                                <input type="text" name="parentContactNumber" placeholder="Enter your contact number">
                             </div>
                             <div class="input-field">
                                 <label>Race</label>
-                                <input type="text" name="parentRace" placeholder="Race" required >
+                                <input type="text" name="parentRace" placeholder="Race">
                             </div>
                             <div class="input-field">
                                 <label>Gender</label>
-                                <input type="text" name="parentGender" placeholder="Enter your gender" required >
+                                <input type="text" name="parentGender" placeholder="Enter your gender">
                             </div>
                             <div class="input-field">
                                 <label>Home Language</label>
-                                <input type="text" name="parentHomeLanguage" placeholder="Home Language" required >
+                                <input type="text" name="parentHomeLanguage" placeholder="Home Language">
                             </div>
                             <div class="input-field">
                                 <label>Relationship</label>
-                                <input type="text" name="parentRelationship" placeholder="Relationship" required >
+                                <input type="text" name="parentRelationship" placeholder="Relationship">
                             </div>
                             <div class="input-field">
                                 <label>Residential Address</label>
-                                <input type="text" name="parentResidentialAddress" placeholder="Residence Address" required >
+                                <input type="text" name="parentResidentialAddress" placeholder="Residence Address">
                             </div>
                             <div class="input-field">
                                 <label>Residential Code</label>
-                                <input type="text" name="parentResidentialCode" placeholder="Residence Code" required >
+                                <input type="text" name="parentResidentialCode" placeholder="Residence Code">
                             </div>
                         </div>
                     </div>
-                    <div class="second parent details id">
+                    <div class="second-parent-details-id">
                         <span class="title">Second Parent/ Guardian Information</span>
 
                         <div class="fields">
                             <div class="input-field">
                                 <label>Title</label>
-                                <input type="text" name="secondParentTitle" placeholder="Enter your title" required >
+                                <input type="text" name="secondParentTitle" placeholder="Enter your title">
                             </div>
                             <div class="input-field">
                                 <label>Initials</label>
-                                <input type="text" name="secondParentInitials" placeholder="Enter your initials" required >
+                                <input type="text" name="secondParentInitials" placeholder="Enter your initials">
                             </div>
                             <div class="input-field">
                                 <label>First Names</label>
-                                <input type="text" name="secondParentFirstNames" placeholder="Enter your first name(s)" required >
+                                <input type="text" name="secondParentFirstNames" placeholder="Enter your first name(s)">
                             </div>
                             <div class="input-field">
                                 <label>Surname</label>
-                                <input type="text" name="secondParentSurname" placeholder="Enter your surname" required >
+                                <input type="text" name="secondParentSurname" placeholder="Enter your surname">
                             </div>
                             <div class="input-field">
                                 <label>ID Number</label>
-                                <input type="text" name="secondParentIdNumber" placeholder="Enter your ID Number" required >
+                                <input type="text" name="secondParentIdNumber" placeholder="Enter your ID Number">
                             </div>
                             <div class="input-field">
                                 <label>Relationship</label>
-                                <input type="text" name="secondParentRelationship" placeholder="Enter your relationship" required >
+                                <input type="text" name="secondParentRelationship" placeholder="Enter your relationship">
                             </div>
                             <div class="input-field">
                                 <label>Gender</label>
-                                <input type="text" name="secondParentGender" placeholder="gender" required >
+                                <input type="text" name="secondParentGender" placeholder="gender">
                             </div>
                             <div class="input-field">
                                 <label>Home Language</label>
-                                <input type="text" name="secondParentHomeLanguage" placeholder="Enter your home language" required >
+                                <input type="text" name="secondParentHomeLanguage" placeholder="Enter your home language">
                             </div>
                             <div class="input-field">
                                 <label>Race</label>
-                                <input type="text" name="secondParentRace" placeholder="race" required >
+                                <input type="text" name="secondParentRace" placeholder="race">
                             </div>
                             <div class="input-field">
                                 <label>Residential Address</label>
-                                <input type="text" name="secondParentResidentialAddress" placeholder="Residence Address" required >
+                                <input type="text" name="secondParentResidentialAddress" placeholder="Residence Address">
                             </div>
                             <div class="input-field">
                             <label>Residential Code</label>
-                                <input type="text" name="secondParentResidentialCode" placeholder="Residence Code" required >
+                                <input type="text" name="secondParentResidentialCode" placeholder="Residence Code">
                             </div>
                             <div class="input-field">
                                 <label>Contact Number</label>
-                                <input type="text" name="secondParentContactNumber" placeholder="contactNumber" required >
+                                <input type="text" name="secondParentContactNumber" placeholder="contactNumber">
                             </div>
                             <div class="input-field">
                                 <label>Upload File</label>
